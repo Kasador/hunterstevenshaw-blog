8779c4c659c91598f7ddc8f2c4831b6dba091763
Kasador <hunterstevenshaw@gmail.com>
2025-04-14 20:54:25 -0700

    Updated Login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export default function Login() {
     return (
-        <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl">Login</h1>
+        <div>
+            <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl">Login</h1>
+            <Link href="/">Back</Link>
+        </div>
     )
 }
