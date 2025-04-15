92bc83839e1fe3f4ab0b4c631a270f285022dde8
Kasador <hunterstevenshaw@gmail.com>
2025-04-14 20:36:30 -0700

    Updated links/routes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 // import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl">Logbook.io</h1>
-      <a href="/login">Login</a>
+      <Link href="/login">Login</Link>
     </div>
   );
 }
