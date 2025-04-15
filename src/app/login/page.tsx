8ba10b24d6ba2791b855372d8f0f2dcd7b4458ea
Kasador <hunterstevenshaw@gmail.com>
2025-04-14 21:09:33 -0700

    Added Shadcn UI components.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import { Button } from "@/components/ui/button"
 
 export default function Login() {
     return (
         <div>
-            <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl">Login</h1>
-            <Link href="/">Back</Link>
+            <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">Login</h1>
+            <Button className='text-2xl m-5 p-5'>
+                <Link href="/">Home</Link>
+            </Button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 // import Image from "next/image";
 import Link from 'next/link'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl">Logbook.io</h1>
-      <Link href="/login">Login</Link>
+      <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">Logbook.io</h1>
+      <Button className='text-2xl m-5 p-5'>
+        <Link href="/login">Login</Link>
+      </Button>
     </div>
   );
 }
