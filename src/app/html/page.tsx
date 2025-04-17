71c953b2eb4e3f85986be0f927bc2a18f49c62c0
Kasador <hunterstevenshaw@gmail.com>
2025-04-16 18:30:25 -0700

    Updated menu items.

--- src/app/html/page.tsx
+++ src/app/html/page.tsx
@@ -16,46 +16,48 @@ import {
 export default function HTML() {
     return (
         <div>
-             <div className="flex mx-2 justify-between">
-      <Link href="/">
-      <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">Logbook.io</h1>
-      </Link>
-      <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/html">
-          <NavigationMenuTrigger>HTML</NavigationMenuTrigger>
-          </Link>
-          <NavigationMenuContent>
-            <NavigationMenuLink className="w-75">Link #1</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/css">
-          <NavigationMenuTrigger>CSS</NavigationMenuTrigger>
-          </Link>
-          <NavigationMenuContent>
-            <NavigationMenuLink className="w-75">Link #2</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-            <Link href="/javascript">
-          <NavigationMenuTrigger>JavaScript</NavigationMenuTrigger>
+            <div className="flex mx-2 justify-between">
+            <Link href="/">
+            <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">Logbook.io</h1>
             </Link>
-          <NavigationMenuContent>
-            <NavigationMenuLink className="w-75">Link #3</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-        <Link href="/login">
-          <NavigationMenuLink className="w-25 text-center text-1xl font-extrabold">
-            Login
-          </NavigationMenuLink>
-        </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-    </div>
+            <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                <Link href="/html">
+                <NavigationMenuTrigger>HTML</NavigationMenuTrigger>
+                </Link>
+                <NavigationMenuContent>
+                    <NavigationMenuLink className="w-75">Forms</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                <Link href="/css">
+                <NavigationMenuTrigger>CSS</NavigationMenuTrigger>
+                </Link>
+                <NavigationMenuContent>
+                    <NavigationMenuLink className="w-75">SASS/SCSS</NavigationMenuLink>
+                    <NavigationMenuLink className="w-75">TailwindCSS</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/javascript">
+                <NavigationMenuTrigger>JavaScript</NavigationMenuTrigger>
+                    </Link>
+                <NavigationMenuContent>
+                    <NavigationMenuLink className="w-75">ReactJS</NavigationMenuLink>
+                    <NavigationMenuLink className="w-75">NextJS</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                <Link href="/login">
+                <NavigationMenuLink className="w-25 text-center text-1xl font-extrabold">
+                    Login
+                </NavigationMenuLink>
+                </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+            </NavigationMenu>
+            </div>
             <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">HTML</h1>
             <Button className='text-2xl m-5 p-5'>
                 <Link href="/">Home</Link>
