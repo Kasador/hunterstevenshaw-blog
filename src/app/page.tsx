2010bbbd79f761c7a3bd8d3830011a04a5ebe804
Kasador <hunterstevenshaw@gmail.com>
2025-04-18 23:44:16 -0700

    🔐 Added user AUTH on sign in/out.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,16 @@ import {
 // https://clerk.com/docs/quickstarts/nextjs-pages-router
 
 export default function Home() {
-
   return (
-    <div>
       <ClerkProvider>
-        <SignedIn>
-          <h1>Welcome, you're signed-in.</h1>
-        </SignedIn>
-        <SignedOut>
-          <h1>Oh no, you're not signed-in.</h1>
-        </SignedOut>
+        <div>
+          <SignedIn>
+            <h1>Welcome, you're signed-in.</h1>
+          </SignedIn>
+          <SignedOut>
+            <h1>Oh no, you're not signed-in.</h1>
+          </SignedOut>
+        </div>
       </ClerkProvider>
-    </div>
   );
 }
