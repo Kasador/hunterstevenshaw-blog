66bcdfee3a02b02e6fcd747b3f97f91353734c05
Kasador <hunterstevenshaw@gmail.com>
2025-04-18 22:14:43 -0700

    Deleted nav menus from components.

--- src/app/html/page.tsx
+++ src/app/html/page.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link'
 export default function HTML() {
     return (
         <div>
-           <Nav />
             <h1 className="font-[family-name:var(--font-dynapuff)] font-extrabold text-4xl p-2">HTML</h1>
             <Button className='text-2xl m-5 p-5'>
                 <Link href="/">Home</Link>
