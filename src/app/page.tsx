5878b0af6fbd6d7873620bb3399d26c0e1d09f49
Kasador <hunterstevenshaw@gmail.com>
2025-04-18 23:47:50 -0700

    🔐 Added user AUTH on sign in/out.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,17 @@ import {
 } from '@clerk/nextjs'
 
 // https://clerk.com/docs/quickstarts/nextjs-pages-router
+// https://stackoverflow.com/questions/43177074/how-can-i-fix-this-violation-of-this-react-no-unescaped-entitie-of-an-eslint-r
 
 export default function Home() {
   return (
       <ClerkProvider>
         <div>
           <SignedIn>
-            <h1>Welcome, you're signed-in.</h1>
+            <h1>Welcome, you&apos;re signed-in.</h1>
           </SignedIn>
           <SignedOut>
-            <h1>Oh no, you're not signed-in.</h1>
+            <h1>Oh no, you&apos;re not signed-in. Sign up or sign in to see user data.</h1>
           </SignedOut>
         </div>
       </ClerkProvider>
